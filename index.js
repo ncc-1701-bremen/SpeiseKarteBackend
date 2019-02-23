517ff86755b853107d4ebbdf9aad46fbfa651d1e
Dustin Gogoll <dustin.gogoll@googlemail.com>
2019-02-23 13:26:52 +0100

    First redis setup

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const PORT = process.env.PORT || 5000;
-const io =  require('socket.io')();
+const io = require('socket.io')();
+const redis =  require('socket.io-redis');
+
+io.adapter(redis({
+  host: '127.0.0.1',
+  port: 6379
+}));
 
 io.on('connection', (client) => {
-  setInterval(() => {
-    client.emit('test', "oh yes!")
-  }, 1000)
+    io.sockets.emit('test', "oh yes!")
 })
 
 io.listen(PORT);
