b9220732123e678a529b16624bc61a3c31b84592
Dustin Gogoll <dustin.gogoll@googlemail.com>
2019-02-24 12:20:28 +0100

    Added password to redis server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const PORT = process.env.PORT || 5000;
+const REDIS_PW = process.env.REDIS_PW;
 const io = require('socket.io')();
 const redis =  require('socket.io-redis');
 
 io.adapter(redis({
   host: '127.0.0.1',
-  port: 6379
+  port: 6379,
+  password: REDIS_PW
 }));
 
 io.on('connection', (client) => {
