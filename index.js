1f96d0e3b536351b669f44dba2aaffab20252e19
Dustin Gogoll <dustin.gogoll@googlemail.com>
2019-02-25 11:26:06 +0100

    Basic server sided auth logic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const REDIS_PW = process.env.REDIS_PW;
 const io = require('socket.io')();
+const authIo = io.of('/authenticate')
 const redis =  require('socket.io-redis');
 
 io.adapter(redis({
@@ -10,8 +11,28 @@ io.adapter(redis({
   password: REDIS_PW
 }));
 
+
+
+require('socketio-auth')(authIo, {
+  authenticate: function (socket, data, callback) {
+    //get credentials sent by the client
+    var username = data.username;
+    var password = data.password;
+    console.log(username, password)
+    return callback(null, true)
+  }
+});
+
 io.on('connection', (client) => {
     io.sockets.emit('test', "oh yes!")
 })
+
+setInterval(()=> {
+  io.sockets.emit('test', "oh yes!")
+}, 1000);
+
+setInterval(()=> {
+  authIo.emit('test', "for auth")
+}, 1000);
 
 io.listen(PORT);
